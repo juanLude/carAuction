8c0b4bfc91486d3bad040eea0ef196deafcba6cd
Juan Ludevid <jmludevid@hotmail.com>
2023-12-14 22:28:17 +1100

    add Status enum

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -1,6 +1,18 @@
-﻿namespace AuctionService;
+﻿namespace AuctionService.Entities;
 
 public class Auction
 {
+    // properties
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public String Seller { get; set;}
+    public String Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt {get;set;}= DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd {get; set;}
+    public Status Status { get; set; }
+    public Item Item { get; set; }
 
 }
