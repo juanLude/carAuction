4ea4abe1dd7884b67b97e82578a7424ee118d910
Juan Ludevid <jmludevid@hotmail.com>
2024-01-07 17:36:22 +1100

    update docker file

---
+++ src/AuctionService/Data/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService;
+
+public class DbInitializer
+{
+    public static void InitDb(WebApplication app){
+        using var scope = app.Services.CreateScope();
+        SeedData(scope.ServiceProvider.GetService<AuctionDbContext>())
+    }
+
+    private static void SeedData(AuctionDbContext conext)
+    {
+        conext.Database.Migrate();
+    }
+}
